edbd3dacd9faeef4e41b06d2307e632cf7824166
mirpedrol <mirp.julia@gmail.com>
2022-08-17 14:19:44 +0200

    fix test param

--- plugins/validation/src/test/nextflow/validation/SchemaValidatorTest.groovy
+++ plugins/validation/src/test/nextflow/validation/SchemaValidatorTest.groovy
@@ -27,7 +27,7 @@ class SchemaValidatorTest extends Specification {
         def validator = new SchemaValidator()
 
         when:
-        def params = [xyz: '/some/path', fail_unrecognised_params: 'true']
+        def params = [xyz: '/some/path']
         validator.validateParameters(params, '/some/path', SCHEMA)
 
         then:
@@ -55,7 +55,7 @@ class SchemaValidatorTest extends Specification {
         def validator = new SchemaValidator()
 
         when:
-        def params = [xyz: '/some/path']
+        def params = [xyz: '/some/path', fail_unrecognised_params: 'true']
         validator.validateParameters(params, '/some/path', SCHEMA)
 
         then:
