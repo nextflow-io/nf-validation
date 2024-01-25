3a978faacc35fa4a8a9c11532ff8851aeab5aee2
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2024-01-25 10:55:34 +0100

    correctly name a file

--- plugins/nf-validation/src/main/nextflow/validation/CustomEvaluators/LenientTypeEvaluator.groovy
+++ plugins/nf-validation/src/main/nextflow/validation/CustomEvaluators/LenientTypeEvaluator.groovy
@@ -14,37 +14,6 @@ import java.util.stream.Collectors
 class LenientTypeEvaluator implements Evaluator {
     // Validate against the type
 
-    // private final List<SimpleType> types
-    // private final List<SimpleType> lenientTypes = [
-    //     SimpleType.STRING, 
-    //     SimpleType.INTEGER, 
-    //     SimpleType.NUMBER, 
-    //     SimpleType.BOOLEAN,
-    //     SimpleType.NULL
-    // ]
-
-    // LenientTypeEvaluator(JsonNode node) {
-    //     if(node.isString()) {
-    //         this.types = [SimpleType.fromName(node.asString())]
-    //     } else {
-    //         this.types = node.asArray().collect { SimpleType.fromName(it.asString()) }
-    //     }
-    // }
-
-    // @Override
-    // public Result evaluate(EvaluationContext ctx, JsonNode node) {
-    //     def SimpleType nodeType = node.getNodeType()
-    //     if (types.contains(SimpleType.STRING) && lenientTypes.contains(nodeType)) {
-    //         // println("Lenient: ${node.asString()}")
-    //         return Result.success()
-    //     } else if (types.contains(nodeType) || nodeType == SimpleType.INTEGER && types.contains(SimpleType.NUMBER)) {
-    //         // println("Not lenient: ${node.asString()}")
-    //         return Result.success()
-    //     } else {
-    //         return Result.failure("Value is [${nodeType.getName()}] but should be ${type}" as String)
-    //     }
-    // }
-
     private final Set<SimpleType> types
     private final List<SimpleType> lenientTypes = [
         SimpleType.STRING, 
