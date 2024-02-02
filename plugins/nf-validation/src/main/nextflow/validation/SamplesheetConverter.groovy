b83d3801deae6bc59b90be0f4cdb776dc65651ef
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2024-02-02 14:13:37 +0100

    support deeply nested samplesheets

--- plugins/nf-validation/src/main/nextflow/validation/SamplesheetConverter.groovy
+++ plugins/nf-validation/src/main/nextflow/validation/SamplesheetConverter.groovy
@@ -33,148 +33,71 @@ import nextflow.Session
 class SamplesheetConverter {
 
     private static List<Map> rows = []
+    private static Map meta = [:]
 
-    static List convertToList(Path samplesheetFile, Path schemaFile, Boolean header) {
+    private static Map getMeta() {
+        this.meta
+    }
 
-        def JSONObject schemaMap = new JSONObject(schemaFile.text)
-        def JSONArray samplesheetList = Utils.fileToJsonArray(samplesheetFile, schemaFile)
+    private static Map resetMeta() {
+        this.meta = [:]
+    }
+
+    private static addMeta(Map newEntries) {
+        this.meta = this.meta + newEntries
+    }
+
+    private static Boolean isMeta() {
+        this.meta.size() > 0
+    }
+
+    public static List convertToList(Path samplesheetFile, Path schemaFile) {
+
+        def LinkedHashMap schemaMap = new JsonSlurper().parseText(schemaFile.text) as LinkedHashMap
+        def List samplesheetList = Utils.fileToList(samplesheetFile, schemaFile)
 
         this.rows = []
 
-        def Iterator<Object> samplesheetIterator = samplesheetList.iterator()
-
-        while (samplesheetIterator.hasNext()) {
-            println(samplesheetIterator.next())
+        def List channelFormat = samplesheetList.collect { entry ->
+            resetMeta()
+            def Object result = formatEntry(entry, schemaMap["items"] as LinkedHashMap)
+            if (result instanceof List) {
+                result.add(0,getMeta())
+            } else if (isMeta()) {
+                result = [getMeta(), result]
+            }
+            return result
         }
-
-        // def List outputs = samplesheetList.collect { fullRow ->
-
-        //     println(fullrow.getClass())
-
-            // def Map<String,Object> row = fullRow.findAll { it.value != "" }
-            // def Set rowKeys = header ? row.keySet() : ["empty"].toSet()
-
-            // Check the header (CSV/TSV) or present fields (YAML)
-            // TODO reimplement warning for unused fields
-
-            // this.rows.add(row)
-
-            // def Map meta = [:]
-            // def ArrayList output = []
-
-            // for( Map.Entry<String, Map> field : schemaFields ){
-            //     def String key = header ? field.key : "empty"
-            //     def Object input = row[key]
-
-            //     // Convert field to a meta field or add it as an input to the channel
-            //     def List<String> metaNames = field['value']['meta'] as List<String>
-            //     if(metaNames) {
-            //         for(name : metaNames) {
-            //             meta[name] = (input != '' && input != null) ?
-            //                     castToNFType(input, field) :
-            //                 field['value']['default'] != null ?
-            //                     castToNFType(field['value']['default'], field) :
-            //                     null
-            //         }
-            //     }
-            //     else {
-            //         def inputVal = (input != '' && input != null) ?
-            //                 castToNFType(input, field) :
-            //             field['value']['default'] != null ?
-            //                 castToNFType(field['value']['default'], field) :
-            //                 []
-            //         output.add(inputVal)
-            //     }
-            // }
-            // // Add meta to the output when a meta field has been created
-            // if(meta != [:]) { output.add(0, meta) }
-        //     return []
-        // }
-
-        return []
+        return channelFormat
     }
 
-    // Function to transform an input field from the samplesheet to its desired type
-    private static castToNFType(
-        Object input,
-        Map.Entry<String, Map> field
-    ) {
-        def String type = field['value']['type']
-        def String key = field.key
+    private static Object formatEntry(Object input, LinkedHashMap schema) {
 
-        // Recursively call this function for each item in the array if the field is an array-type
-        // The returned values are collected into a single array
-        if (type == "array") {
-            def Map.Entry<String, Map> subfield = (Map.Entry<String, Map>) Map.entry(field.key, field['value']['items'])
-            log.debug "subfield = $subfield"
-            def ArrayList result = input.collect{ castToNFType(it, subfield) } as ArrayList
-            return result
-        }
-
-        def String inputStr = input as String
-        // Convert string values
-        if(type == "string" || !type) {
-            def String result = inputStr as String
-            
-            // Check and convert to the desired format
-            def String format = field['value']['format']
-            if(format) {
-                if(format == "file-path-pattern") {
-                    def ArrayList inputFiles = Nextflow.file(inputStr) as ArrayList
-                    return inputFiles
-                }
-                if(format.contains("path")) {
-                    def Path inputFile = Nextflow.file(inputStr) as Path
-                    return inputFile
+        if (input instanceof Map) {
+            def List result = []
+            def LinkedHashMap properties = schema["properties"]
+            properties.each { property, schemaValues ->
+                def value = input[property] ?: []
+                def List metaIds = schemaValues["meta"] instanceof List ? schemaValues["meta"] as List : schemaValues["meta"] instanceof String ? [schemaValues["meta"]] : []
+                if (metaIds) {
+                    metaIds.each {
+                        addMeta(["${it}":value])
+                    }
+                } else {
+                    result += [formatEntry(value, schemaValues as LinkedHashMap)]                
                 }
             }
-            
-
-            // Return the plain string value
             return result
-        }
-
-        // Convert number values
-        else if(type == "number") {
-            try {
-                def int result = inputStr as int
-                return result
+        } else if (input instanceof List) {
+            def List result = []
+            input.each {
+                result += [formatEntry(it, schema["items"] as LinkedHashMap)]
             }
-            catch (NumberFormatException e) {
-                log.debug("Could not convert ${input} to an integer. Trying to convert to a float.")
-            }
-
-            try {
-                def float result = inputStr as float
-                return result
-            }
-            catch (NumberFormatException e) {
-                log.debug("Could not convert ${inputStr} to a float. Trying to convert to a double.")
-            }
-            
-            def double result = inputStr as double
             return result
+        } else {
+            return input
         }
 
-        // Convert integer values
-        else if(type == "integer") {
-
-            def int result = inputStr as int
-            return result
-        }
-
-        // Convert boolean values
-        else if(type == "boolean") {
-
-            if(inputStr.toLowerCase() == "true") {
-                return true
-            }
-            return false
-        }
-
-        else if(type == "null") {
-            return null
-        }
     }
 
 }
