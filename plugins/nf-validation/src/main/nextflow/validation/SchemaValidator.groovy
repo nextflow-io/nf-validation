825219cc480e6423b1bd0b1732a0f224f8664a21
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2024-02-13 14:32:13 +0100

    update the error message for fromSamplesheet

--- plugins/nf-validation/src/main/nextflow/validation/SchemaValidator.groovy
+++ plugins/nf-validation/src/main/nextflow/validation/SchemaValidator.groovy
@@ -176,9 +176,12 @@ class SchemaValidator extends PluginExtensionPoint {
             log.error(
                 """ Following error has been found during samplesheet conversion:
     ${e}
+    ${e.getStackTrace().join("\n\t")}
 
 Please run validateParameters() first before trying to convert a samplesheet to a channel.
 Reference: https://nextflow-io.github.io/nf-validation/parameters/validation/
+
+Also make sure that the same schema is used for validation and conversion of the samplesheet
 """ as String
             )
         }
