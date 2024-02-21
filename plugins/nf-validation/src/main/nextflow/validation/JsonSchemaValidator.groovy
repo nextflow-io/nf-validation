d5a1729b0c9ef35fb012be483c3c4b2b5598f1df
Júlia Mir Pedrol <mirp.julia@gmail.com>
2024-02-21 15:33:49 +0100

    Apply suggestions from code review
    Co-authored-by: Matthias Hörtenhuber <mashehu@users.noreply.github.com>

--- plugins/nf-validation/src/main/nextflow/validation/JsonSchemaValidator.groovy
+++ plugins/nf-validation/src/main/nextflow/validation/JsonSchemaValidator.groovy
@@ -39,7 +39,9 @@ public class JsonSchemaValidator {
             log.error("""Failed to load the meta schema:
     The used schema draft (${draft}) is not correct, please use \"https://json-schema.org/draft/2020-12/schema\" instead.
         - If you are a pipeline developer, check our migration guide for more information: https://nextflow-io.github.io/nf-validation/latest/migration_guide/
-        - If you are a pipeline user, pin the previous version of the plugin (1.1.3) to avoid this error: https://www.nextflow.io/docs/latest/plugins.html#using-plugins
+        - If you are a pipeline user, pin the previous version of the plugin (1.1.3) to avoid this error: https://www.nextflow.io/docs/latest/plugins.html#using-plugins, i.e. set `plugins {
+    id 'nf-validation@1.1.3'
+}` in your `nextflow.config` file
             """)
             throw new SchemaValidationException("", [])
         }
