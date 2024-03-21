ba91212e272ee31c9332955426a5736fa4771f71
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2024-03-21 09:33:30 +0100

    move as much logic as possible to the SamplesheetConverter

--- plugins/nf-schema/src/main/nextflow/validation/SchemaValidator.groovy
+++ plugins/nf-schema/src/main/nextflow/validation/SchemaValidator.groovy
@@ -143,23 +143,9 @@ class SchemaValidator extends PluginExtensionPoint {
     ) {
         def params = session.params
         final target = CH.createBy(source)
-        final validator = new JsonSchemaValidator()
         final next = {
-            def JSONArray samplesheet = Utils.fileToJsonArray(it as Path, schema)
-            def List<String> validationErrors = validator.validate(samplesheet, schema.text)
-            this.errors.addAll(validationErrors)
-            if (validationErrors) {
-                def Boolean useMonochromeLogs = options?.containsKey('monochrome_logs') ? options.monochrome_logs as Boolean :
-                    params.monochrome_logs ? params.monochrome_logs as Boolean : 
-                    params.monochromeLogs  ? params.monochromeLogs as Boolean :
-                    false
-                def colors = logColours(useMonochromeLogs)
-                def msg = "${colors.red}The following errors have been detected in ${it.toString()}:\n\n" + validationErrors.join('\n').trim() + "\n${colors.reset}\n"
-                log.error("Validation of samplesheet failed!")
-                throw new SchemaValidationException(msg, this.getErrors())
-            }
-            def SamplesheetConverter converter = new SamplesheetConverter(it as Path, schema)
-            def List arrayChannel = converter.convertToList()
+            def SamplesheetConverter converter = new SamplesheetConverter(it as Path, schema, params, options)
+            def List arrayChannel = converter.validateAndConvertToList()
             arrayChannel.each { 
                 target.bind(it)
             }
@@ -304,7 +290,7 @@ class SchemaValidator extends PluginExtensionPoint {
         }
 
         // Colors
-        def colors = logColours(useMonochromeLogs)
+        def colors = Utils.logColours(useMonochromeLogs)
 
         // Validate
         List<String> validationErrors = validator.validate(paramsJSON, schema_string)
@@ -373,7 +359,7 @@ class SchemaValidator extends PluginExtensionPoint {
             params.monochromeLogs  ? params.monochromeLogs as Boolean :
             false
 
-        def colors = logColours(useMonochromeLogs)
+        def colors = Utils.logColours(useMonochromeLogs)
         Integer num_hidden = 0
         String output  = ''
         output        += 'Typical pipeline command:\n\n'
@@ -553,7 +539,7 @@ class SchemaValidator extends PluginExtensionPoint {
             params.monochromeLogs  ? params.monochromeLogs as Boolean :
             false
 
-        def colors = logColours(useMonochromeLogs)
+        def colors = Utils.logColours(useMonochromeLogs)
         String output  = ''
         def LinkedHashMap params_map = paramsSummaryMap(workflow, parameters_schema: schemaFilename)
         def max_chars  = paramsMaxChars(params_map)
@@ -690,73 +676,5 @@ class SchemaValidator extends PluginExtensionPoint {
             }
         }
         return max_chars
-    }
-
-    //
-    // ANSII Colours used for terminal logging
-    //
-    private static Map logColours(Boolean monochrome_logs) {
-        Map colorcodes = [:]
-
-        // Reset / Meta
-        colorcodes['reset']      = monochrome_logs ? '' : "\033[0m"
-        colorcodes['bold']       = monochrome_logs ? '' : "\033[1m"
-        colorcodes['dim']        = monochrome_logs ? '' : "\033[2m"
-        colorcodes['underlined'] = monochrome_logs ? '' : "\033[4m"
-        colorcodes['blink']      = monochrome_logs ? '' : "\033[5m"
-        colorcodes['reverse']    = monochrome_logs ? '' : "\033[7m"
-        colorcodes['hidden']     = monochrome_logs ? '' : "\033[8m"
-
-        // Regular Colors
-        colorcodes['black']      = monochrome_logs ? '' : "\033[0;30m"
-        colorcodes['red']        = monochrome_logs ? '' : "\033[0;31m"
-        colorcodes['green']      = monochrome_logs ? '' : "\033[0;32m"
-        colorcodes['yellow']     = monochrome_logs ? '' : "\033[0;33m"
-        colorcodes['blue']       = monochrome_logs ? '' : "\033[0;34m"
-        colorcodes['purple']     = monochrome_logs ? '' : "\033[0;35m"
-        colorcodes['cyan']       = monochrome_logs ? '' : "\033[0;36m"
-        colorcodes['white']      = monochrome_logs ? '' : "\033[0;37m"
-
-        // Bold
-        colorcodes['bblack']     = monochrome_logs ? '' : "\033[1;30m"
-        colorcodes['bred']       = monochrome_logs ? '' : "\033[1;31m"
-        colorcodes['bgreen']     = monochrome_logs ? '' : "\033[1;32m"
-        colorcodes['byellow']    = monochrome_logs ? '' : "\033[1;33m"
-        colorcodes['bblue']      = monochrome_logs ? '' : "\033[1;34m"
-        colorcodes['bpurple']    = monochrome_logs ? '' : "\033[1;35m"
-        colorcodes['bcyan']      = monochrome_logs ? '' : "\033[1;36m"
-        colorcodes['bwhite']     = monochrome_logs ? '' : "\033[1;37m"
-
-        // Underline
-        colorcodes['ublack']     = monochrome_logs ? '' : "\033[4;30m"
-        colorcodes['ured']       = monochrome_logs ? '' : "\033[4;31m"
-        colorcodes['ugreen']     = monochrome_logs ? '' : "\033[4;32m"
-        colorcodes['uyellow']    = monochrome_logs ? '' : "\033[4;33m"
-        colorcodes['ublue']      = monochrome_logs ? '' : "\033[4;34m"
-        colorcodes['upurple']    = monochrome_logs ? '' : "\033[4;35m"
-        colorcodes['ucyan']      = monochrome_logs ? '' : "\033[4;36m"
-        colorcodes['uwhite']     = monochrome_logs ? '' : "\033[4;37m"
-
-        // High Intensity
-        colorcodes['iblack']     = monochrome_logs ? '' : "\033[0;90m"
-        colorcodes['ired']       = monochrome_logs ? '' : "\033[0;91m"
-        colorcodes['igreen']     = monochrome_logs ? '' : "\033[0;92m"
-        colorcodes['iyellow']    = monochrome_logs ? '' : "\033[0;93m"
-        colorcodes['iblue']      = monochrome_logs ? '' : "\033[0;94m"
-        colorcodes['ipurple']    = monochrome_logs ? '' : "\033[0;95m"
-        colorcodes['icyan']      = monochrome_logs ? '' : "\033[0;96m"
-        colorcodes['iwhite']     = monochrome_logs ? '' : "\033[0;97m"
-
-        // Bold High Intensity
-        colorcodes['biblack']    = monochrome_logs ? '' : "\033[1;90m"
-        colorcodes['bired']      = monochrome_logs ? '' : "\033[1;91m"
-        colorcodes['bigreen']    = monochrome_logs ? '' : "\033[1;92m"
-        colorcodes['biyellow']   = monochrome_logs ? '' : "\033[1;93m"
-        colorcodes['biblue']     = monochrome_logs ? '' : "\033[1;94m"
-        colorcodes['bipurple']   = monochrome_logs ? '' : "\033[1;95m"
-        colorcodes['bicyan']     = monochrome_logs ? '' : "\033[1;96m"
-        colorcodes['biwhite']    = monochrome_logs ? '' : "\033[1;97m"
-
-        return colorcodes
     }
 }
